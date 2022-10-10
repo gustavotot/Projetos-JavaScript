ab437270c9105b75847d31df0c710f637f6cd2a1
Gustavo Tot <95966202+gustavotot@users.noreply.github.com>
2022-10-10 01:23:16 +0100

    Novo projeto Cronometro

---
+++ project_02/script.js
@@ -0,0 +1,49 @@
+
+
+var ss = 00;
+var mm = 00;
+var hh = 00;
+
+var time = 1000;
+var cron;
+
+var start_btn = document.getElementById("start_btn");
+var pause_btn = document.getElementById("pause_btn");
+var stop_btn = document.getElementById("stop_btn");
+function start(){
+    cron = setInterval(timer,time)
+    start_btn.disabled = true;
+    pause_btn.disabled = false;
+    stop_btn.disabled = false;
+}
+function pause(){
+    clearInterval(cron)
+    start_btn.disabled = false;
+    pause_btn.disabled = true;
+}
+function stop(){
+    clearInterval(cron)
+    ss = 0;
+    mm = 0;
+    hh = 0;
+    document.getElementById("cron").innerText = `00:00:00`
+
+    start_btn.disabled = false;
+    pause_btn.disabled = true;
+    stop_btn.disabled = true;
+}
+
+function timer(){
+    ss++;
+    if(ss == 60){
+        mm++;
+        ss = 0;
+
+        if(mm == 60){
+            hh++;
+            mm = 0;
+        }
+    }
+    var format = (hh < 10 ? '0'+hh : hh) + ':' + (mm < 10 ? '0'+mm : mm) + ':' + (ss < 10 ? '0'+ss : ss);
+    document.getElementById("cron").innerText = format
+}
