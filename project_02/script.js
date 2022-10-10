7dd4749b5381da82180ce15363aa5077dff15d65
Gustavo Tot <95966202+gustavotot@users.noreply.github.com>
2022-10-10 11:16:31 +0100

    Milissegundos

--- project_02/script.js
+++ project_02/script.js
@@ -1,10 +1,10 @@
 
-
+var ms = 00;
 var ss = 00;
 var mm = 00;
 var hh = 00;
 
-var time = 1000;
+var time = 1000/60;
 var cron;
 
 var start_btn = document.getElementById("start_btn");
@@ -23,10 +23,11 @@ function pause(){
 }
 function stop(){
     clearInterval(cron)
+    ms = 0;
     ss = 0;
     mm = 0;
     hh = 0;
-    document.getElementById("cron").innerText = `00:00:00`
+    document.getElementById("cron").innerText = `00:00:00:00`
 
     start_btn.disabled = false;
     pause_btn.disabled = true;
@@ -34,16 +35,21 @@ function stop(){
 }
 
 function timer(){
-    ss++;
-    if(ss == 60){
-        mm++;
-        ss = 0;
+    ms++;
+    if(ms == 60){
+        ss++;
+        ms = 0;
 
-        if(mm == 60){
-            hh++;
-            mm = 0;
+        if(ss == 60){
+            mm++;
+            ss = 0;
+
+            if(mm == 60){ 
+                hh++;
+                mm = 0;
+            }
         }
     }
-    var format = (hh < 10 ? '0'+hh : hh) + ':' + (mm < 10 ? '0'+mm : mm) + ':' + (ss < 10 ? '0'+ss : ss);
+    var format = (hh < 10 ? '0'+hh : hh) + ':' + (mm < 10 ? '0'+mm : mm) + ':' + (ss < 10 ? '0'+ss : ss) + ':' + (ms < 10 ? '0'+ms : ms);
     document.getElementById("cron").innerText = format
 }
